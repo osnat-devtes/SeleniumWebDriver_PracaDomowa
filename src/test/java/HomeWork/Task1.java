c950a1a483fabd6df9d2b21fc2051f1faff78758
osnat-devtes <osnat80091@gmail.com>
2021-11-21 18:22:18 +0000

    Task1

--- src/test/java/HomeWork/Task1.java
+++ src/test/java/HomeWork/Task1.java
@@ -1,7 +1,20 @@
 package HomeWork;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task1 {
     public static void main(String[]args){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(" https://mystore-testlab.coderslab.pl/index.php");
 
+        driver.findElement(By.xpath("//*[@id=\"_desktop_user_info\"]/div/a/span")).click();
+        driver.findElement(By.xpath("//*[@id=\"content\"]/div/a")).click();
+        driver.navigate().back();
+        driver.navigate().back();
     }
 }
